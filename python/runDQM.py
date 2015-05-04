778760c71f6ac4292df4ccd7ef8ae2c99b1c6d20
Devin Taylor <dntaylor@wisc.edu>
2015-05-04 05:58:30 -0500

    adding run script

---
+++ python/runDQM.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from gitUtilities import *
+
+def parse_command_line(argv):
+    parser = argparse.ArgumentParser(description="Run DQM")
+
+    args = parser.parse_args(argv)
+    return args
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    args = parse_command_line(argv)
+
+    begin_dqm()
+
+    return 0
+
+if __name__ == "__main__":
+    main()
